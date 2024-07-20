515279735246b0c365ee5ebe4d277f071955137d
pintu <pkrrp829468@gmail.com>
2024-07-20 09:42:13 +0530

    some changes have made

---
+++ script.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+  const result = a + b;
+  console.log(result);
+}
+sum(4, 8);
+
+const greet = () => {
+  const name = "Mr. Pintu KUmar Ram";
+  console.log(`Hello  ${name}`);
+};
+greet();
+
+document.querySelector(".greet");
+greet.innerText = "Hello Pintu Kumar Ram";
